e63f70231a49822adc4fc31a238b56fc45ae2a02
Olayinka Ogunbiyi <97066655+Olayinka247@users.noreply.github.com>
2023-08-22 16:12:54 +0100

    Add todo, rendering

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-let todos = [];
-todos.push("Clear The Garden");
+const todos = ["Wash Cloth", "Do The Dishes"];
+render(); // to render what we already have in the array before the push
 
-todos.forEach((todosList) => {
-  let element = document.createElement("div");
-  element.innerText = todosList;
-  document.body.appendChild(element);
-});
+function addTodo() {
+  const todoInput = document.getElementById("input-todo");
+  const textInfo = todoInput.value; // to get whatever its typed in the textbox
+  todos.push(textInfo); // push it into the array/ adding a new list of todos
+  render();
+}
+
+function render() {
+  document.getElementById("todo-list").innerHTML = ""; // this is to always clear the previous and not duplicate it when rendering a new one
+  todos.forEach((todoTitle) => {
+    const element = document.createElement("div");
+    element.innerText = todoTitle;
+    const todoList = document.getElementById("todo-list");
+    todoList.appendChild(element);
+  });
+}
